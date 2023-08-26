8f0f137685e93f1a0f7c6b51ef9b86adf42f3508
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-08-26 10:30:23 +0700

    project start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen bg-slate-800 flex justify-center items-center'>
+      <div className="h-24 w-24 bg-slate-700 rounded-lg">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
